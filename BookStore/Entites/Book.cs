a11513318943d7d162358a16c565c297386b8d20
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2023-02-03 20:49:03 +0300

    Refactoring is Done

--- BookStore/Entites/Book.cs
+++ BookStore/Entites/Book.cs
@@ -1,15 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore;
 
 public class Book
 {
-      
 
+    [Required]
     public int Id { get; set; }
 
+    [Required]
     public string Title{get;set;}
+
     public string? Summary{get;set;}
-
-
 
     public DateTime PublicationDate{get;set;} = DateTime.Now;
 
